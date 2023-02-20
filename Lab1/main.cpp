8b534b011bed2e6d110b0ebb978d774c99af069c
Robert <robertolariu0@gmail.com>
2023-02-20 19:38:21 +0200

    msg

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,6 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdint.h>
 #include <iostream>
+using namespace std;
+
+int str_to_int(char *str) {
+    int result = 0;
+    for (int i = 0; i < strlen(str)-1; i++) {
+        result = result * 10 + (str[i] - '0');
+    }
+    return result;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    FILE* file;
+    int sum = 0;
+    file = fopen("/Users/olariurobert/projects/oopE3/oopE3/Lab1/ini.txt", "r");
+    if (file == NULL)
+    {
+        printf("Eroare. Nu am putut deschide fisierul\n");
+    }
+    else {
+        printf("Am deschis fisierul cu success!\n");
+        char line[200];
+        while (fgets(line, 100, file) != NULL) {
+            sum += str_to_int(line);
+        }
+    }
+    printf("%d", sum);
+    printf("\n\n");
 }
