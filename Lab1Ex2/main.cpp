ba1a12f3bb06bd5786fa33c3322e771c021056e7
Robert <robertolariu0@gmail.com>
2023-02-22 08:23:13 +0200

    msg

---
+++ Lab1Ex2/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <cstring>
+int main() {
+    char sen[];
+    char *p;
+    scanf("%[^\n]%*c",sen);
+    p=strtok(sen," ");
+    while(p){
+        
+    }
+
+    return 0;
+}
