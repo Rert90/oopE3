a0d073625a58d5526fec9dfe05eeb6fe95b8f126
Robert <robertolariu0@gmail.com>
2023-03-19 20:51:16 +0200

    msg

--- Lab5/Lab5ex1/Number.cpp
+++ Lab5/Lab5ex1/Number.cpp
@@ -1,7 +1,84 @@
-//
-// Created by Olariu Robert on 18.03.2023.
-//
-
 #include "Number.h"
-Number::Number(const char *value, int base) {
+#include <cstring>
+#include <iostream>
+#include "cmath"
+
+Number::Number(const char* value, int base)
+        : m_base(base)
+{
+    int length = strlen(value);
+    m_value = new char[length + 1];
+    strcpy_s(m_value, length + 1, value);
+    m_digitsCount = length;
+}
+
+Number::Number(const Number& other)
+        : m_base(other.m_base)
+{
+    int length = strlen(other.m_value);
+    m_value = new char[length + 1];
+    strcpy_s(m_value, length + 1, other.m_value);
+    m_digitsCount = other.m_digitsCount;
+}
+
+Number::Number(Number&& other) noexcept
+        : m_value(other.m_value)
+        , m_base(other.m_base)
+        , m_digitsCount(other.m_digitsCount)
+{
+    other.m_value = nullptr;
+    other.m_base = 0;
+    other.m_digitsCount = 0;
+}
+
+Number::~Number()
+{
+    delete[] m_value;
+}
+
+Number& Number::operator=(const Number& other)
+{
+    if (this != &other) {
+        delete[] m_value;
+        m_base = other.m_base;
+        int length = strlen(other.m_value);
+        m_value = new char[length + 1];
+        strcpy_s(m_value, length + 1, other.m_value);
+        m_digitsCount = other.m_digitsCount;
+    }
+    return *this;
+}
+
+Number& Number::operator=(Number&& other) noexcept
+{
+    if (this != &other) {
+        delete[] m_value;
+        m_value = other.m_value;
+        m_base = other.m_base;
+        m_digitsCount = other.m_digitsCount;
+        other.m_value = nullptr;
+        other.m_base = 0;
+        other.m_digitsCount = 0;
+    }
+    return *this;
+}
+
+Number Number::operator+(const Number& other) const
+{
+    // Convert both numbers to base 10
+    int sum = strtol(m_value, nullptr, m_base) + strtol(other.m_value, nullptr, other.m_base);
+    // Convert the sum back to the original base
+    char buffer[33];
+    itoa(sum, buffer, m_base);
+    return Number(buffer, m_base);
+}
+
+Number Number::operator-(const Number& other) const
+{
+    // Convert both numbers to base 10
+    int diff = strtol(m_value, nullptr, m_base) - strtol(other.m_value, nullptr, other.m_base);
+    // Convert the difference back to the original base
+    char buffer[33];
+    itoa(diff, buffer, m_base);
+    return Number(buffer, m_base);
 }
--- Lab5/Lab5ex1/Number.h
+++ Lab5/Lab5ex1/Number.h
@@ -7,18 +7,35 @@
 #pragma once
 
 class Number {
-
-
 public:
-    Number(const char  * value, int base);
+    Number(const char* value, int base);
+    Number(const Number& other);
+    Number(Number&& other) noexcept;
     ~Number();
 
+    Number& operator=(const Number& other);
+    Number& operator=(Number&& other) noexcept;
+    Number operator+(const Number& other) const;
+    Number operator-(const Number& other) const;
+    char operator[](int index) const;
+    bool operator>(const Number& other) const;
+    bool operator<(const Number& other) const;
+    bool operator>=(const Number& other) const;
+    bool operator<=(const Number& other) const;
+    bool operator==(const Number& other) const;
 
     void SwitchBase(int newBase);
-    void Print();
-    int GetDigitsCount();
-    int GetBase();
+    void Print() const;
+    int GetDigitsCount() const;
+    int GetBase() const;
+
+private:
+    char* m_value;
+    int m_base;
+    int m_digitsCount;
 };
+
+
 
 
 #endif //LAB5EX1_NUMBER_H
