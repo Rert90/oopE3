49a67b3d8f94595cd64ba0be359143c0acf997b3
Robert <robertolariu0@gmail.com>
2023-04-03 12:30:31 +0300

    msg

--- Lab7/Lab7Ex1/main.cpp
+++ Lab7/Lab7Ex1/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 
-float operator""_Kelvin(unsigned long long int  temp) {
+float operator""_Kelvin(long double temp) {
     return static_cast<float>(temp - 273.15);
 }
-float operator""_Fahrenheit(unsigned long long int temp) {
+float operator""_Fahrenheit(long double temp) {
     return static_cast<float>((temp - 32) * 5 / 9);
 }
 
 int main() {
-    float a = 300_Kelvin;
-    float b = 120_Fahrenheit;
+    float a = 300.0_Kelvin;
+    float b = 120.0_Fahrenheit;
     printf("From Kelvin to Celsius: %f \n",a);
     printf("From Fahrenheit to Celsius: %f",b);
     return 0;
